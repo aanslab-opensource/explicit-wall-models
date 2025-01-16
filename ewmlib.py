2225fac1029ff67ffdbf4f39a7b5c4139cb71ef3
Roberto <roberto.nuca@kaust.edu.sa>
2025-01-16 18:43:08 +0300

    updated library

--- ewmlib.py
+++ ewmlib.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy.special import lambertw
 from scipy.integrate import quad
+from scipy.optimize import differential_evolution
+import scipy.optimize as optimize
 
 
 def objfun_pwrel(params, rey, up_ref, kappa, B, delta_model):
@@ -48,3 +50,273 @@ class laws:
     @staticmethod
     def up_CaiSagaut_m2_var(Rey, kappa, B, p, s):
         return laws.up_CaiSagaut_m2(Rey, kappa, np.exp(kappa*B), p, s)
+
+#######################################################################################################################
+
+def opti_global_eqode(delta_model, objective_function, bounds):
+    
+    kappa, Aplus = np.meshgrid(
+        np.linspace(0.38, 0.42, 20),
+        np.linspace(  15,   19, 20),
+        indexing='ij'
+        )
+    
+    yp_REF = np.logspace(-1, 4, 500)
+    
+    results = np.empty_like(kappa, dtype=object)
+    
+    B = np.zeros_like(kappa, dtype=np.float64)
+    
+    yp_LOG = np.linspace(5000, 35000, 100)
+
+    progress = 0
+    
+    kappa_size = kappa.size
+    
+    for idx, _ in np.ndenumerate(kappa):
+        progress += 1
+        
+        B[idx] = np.mean(laws.up_ODE(yp_LOG, kappa[idx], Aplus[idx]) - (1 / kappa[idx]) * np.log(yp_LOG))
+        
+        up_REF = laws.up_ODE(yp_REF, kappa[idx], Aplus[idx])
+        
+        rey_REF = up_REF * yp_REF
+
+        result = optimize.shgo(
+            func=objective_function,
+            bounds=bounds,
+            args=(rey_REF, up_REF, kappa[idx], B[idx], delta_model),
+            constraints=None,
+            n=200,
+            iters=1,
+            callback=None, 
+            minimizer_kwargs=None, 
+            options=None, 
+            sampling_method='simplicial', #'halton' 'sobol'
+            workers=1
+            )
+
+        print(f'fun={result.fun:.3e}  residuals={np.round(result.x, 2)}')
+        
+        results[idx] = result
+
+        print(f'{progress} / {kappa_size}', end='\r')
+
+    return kappa, B, Aplus, results, yp_REF
+
+
+def opti_global_reichardt_fixedB1B2(delta_model, objective_function, bounds):
+    
+    B1 = 11
+    B2 = 3
+    
+    kappa, C = np.meshgrid(
+        np.linspace(0.38, 0.40, 20), # reduced range
+        np.linspace(6.80, 7.70, 20),
+        indexing='ij'
+        )
+    
+    yp_REF = np.logspace(-2, 4, 500)
+    
+    results = np.empty_like(kappa, dtype=object)
+    
+    B = np.zeros_like(kappa, dtype=np.float64)
+
+    progress = 0
+    
+    kappa_size = kappa.size
+    
+    for idx, _ in np.ndenumerate(kappa):
+        progress += 1
+                    
+        B[idx] = C[idx] + (np.log(kappa[idx]) / kappa[idx])
+    
+        up_REF  = laws.up_Reichardt(yp_REF, kappa[idx], B1, B2, C[idx])
+
+        rey_REF = up_REF * yp_REF
+
+        result = optimize.shgo(
+            func=objective_function,
+            bounds=bounds,
+            args=(rey_REF, up_REF, kappa[idx], B[idx], delta_model),
+            constraints=None,
+            n=200,
+            iters=1,
+            callback=None, 
+            minimizer_kwargs=None, 
+            options=None, 
+            sampling_method='simplicial', #'halton' 'sobol'
+            workers=1
+            )
+        
+        print(f'fun={result.fun:.3e}  residuals={np.round(result.x, 2)}')
+        
+        results[idx] = result
+                
+        print(f'{progress} / {kappa_size}', end='\r')
+    
+    return kappa, B, C, results, yp_REF
+
+
+def opti_global_spalding(delta_model, objective_function, bounds):
+    
+    kappa, B = np.meshgrid(
+        np.linspace(0.38, 0.42, 20),
+        np.linspace(4.20, 5.50, 20), 
+        indexing='ij'
+        )
+    
+    up_REF = np.logspace(-1, np.log10(50), 500)
+    
+    results = np.empty_like(kappa, dtype=object)
+
+    progress = 0
+    
+    kappa_size = kappa.size
+    
+    for idx, _ in np.ndenumerate(kappa):
+        progress += 1
+                
+        yp_REF = laws.yp_Spalding(up_REF, kappa[idx], B[idx])
+        
+        rey_REF = up_REF * yp_REF
+
+        result = optimize.shgo(
+            func=objective_function,
+            bounds=bounds,
+            args=(rey_REF, up_REF, kappa[idx], B[idx], delta_model),
+            constraints=None,
+            n=200,
+            iters=1,
+            callback=None, 
+            minimizer_kwargs=None, 
+            options=None, 
+            sampling_method='simplicial', #'halton' 'sobol'
+            workers=1
+            )
+        
+        print(f'fun={result.fun:.3e}  residuals={np.round(result.x, 2)}')
+        
+        results[idx] = result
+
+        print(f'{progress} / {kappa_size}', end='\r')
+        
+    return kappa, B, results, up_REF
+
+#######################################################################################################################
+
+def opti_fixedpms_eqode(delta_model, objective_function, bounds, mode):
+    
+    if mode=="highre": # High-re constants
+        kappa = 0.387
+        Aplus = 15.2516
+    elif mode=="classical": # Classical constants    
+        kappa = 0.41
+        Aplus = 17
+    else:
+        exit("ERROR: no constant type specified")
+  
+    yp_ref = np.logspace(-1, 4, 3000)
+
+    yp_log = np.linspace(5000, 35000, 100)
+    
+    up_log = laws.up_ODE(yp_log, kappa, Aplus)       
+    
+    B = np.mean(up_log - (1 / kappa) * np.log(yp_log))
+        
+    up_eqode = laws.up_ODE(yp_ref, kappa, Aplus)
+    
+    rey_eqode = up_eqode * yp_ref
+    
+    results = differential_evolution(
+                func=objective_function,
+                strategy='best1bin',
+                bounds=bounds,
+                args=(rey_eqode, up_eqode, kappa, B, delta_model),
+                workers=32,
+                tol=1e-6,
+                maxiter=2000,
+                popsize=100,
+                polish=True,
+                updating='deferred'
+                )
+    
+    print(results)
+
+    return kappa, B, Aplus, results, yp_ref
+    
+
+def opti_fixedpms_reichardt_fixedB1B2(delta_model, objective_function, bounds, mode):
+
+    if mode=="highre": # High-re constants
+        kappa = 0.387
+        B = 4.21
+        C = B - (np.log(kappa) / kappa)
+    elif mode=="classical": # Classical constants    
+        kappa = 0.41
+        C = 7.8
+        B = C + np.log(kappa) / kappa
+    else:
+        exit("ERROR: no constant type specified")
+
+    B1 = 11
+    B2 = 3
+
+    yp_ref = np.logspace(-1, 4.5, 1000)
+
+    up_rh  = laws.up_Reichardt(yp_ref, kappa, B1, B2, C)
+
+    rey_rh = up_rh * yp_ref
+
+    results = differential_evolution(
+        func=objective_function,
+        strategy='best1bin',
+        bounds=bounds,
+        args=(rey_rh, up_rh, kappa, B, delta_model),
+        workers=32,
+        maxiter=2000,
+        popsize=150,
+        tol=1e-5,
+        atol=1e-5,
+        polish=True,
+        updating='deferred'
+    )
+    
+    print(results)
+    
+    return kappa, B, C, B1, B2, results, yp_ref, up_rh
+
+
+def opti_fixedpms_spalding(delta_model, objective_function, bounds, mode):
+
+    if mode=="highre": # High-re constants
+        kappa = 0.387
+        B = 4.21
+    elif mode=="classical": # Classical constants    
+        kappa = 0.4
+        B = 5.5
+    else:
+        exit("ERROR: no constant type specified")
+    
+    up_ref = np.logspace(-1, np.log10(35), 1000)
+    
+    yp_ref = laws.yp_Spalding(up_ref, kappa, B)
+    
+    rey_spalding = up_ref * yp_ref
+
+    results = differential_evolution(
+        func=objective_function,
+        strategy='best1bin',
+        bounds=bounds,
+        args=(rey_spalding, up_ref, kappa, B, delta_model),
+        workers=10,
+        tol=1e-6,
+        maxiter=2000,
+        popsize=100,
+        polish=True,
+        updating='deferred'
+        )
+    
+    print(results)
+
+    return kappa, B, results, up_ref, yp_ref
